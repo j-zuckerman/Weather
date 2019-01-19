2db16f71513cd75932e5a15fae1007e943d253e4
James Zuckerman <jzuckerman4@gmail.com>
2019-01-18 19:01:12 -0700

    Updates

---
+++ precache-manifest.254db4d0797ccacf83fa0f007c165cc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cba2d3fb97fb33ba4c0",
+    "url": "/Weather/static/js/runtime~main.4cba2d3f.js"
+  },
+  {
+    "revision": "0caf50aa5fdb51fd5714",
+    "url": "/Weather/static/js/main.0caf50aa.chunk.js"
+  },
+  {
+    "revision": "60a0bfde6b12804e9a36",
+    "url": "/Weather/static/js/1.60a0bfde.chunk.js"
+  },
+  {
+    "revision": "0caf50aa5fdb51fd5714",
+    "url": "/Weather/static/css/main.7cd63367.chunk.css"
+  },
+  {
+    "revision": "40201ed8b7e8b02c4804db0374f7ada5",
+    "url": "/Weather/index.html"
+  }
+];
